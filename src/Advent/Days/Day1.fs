0370d3c64a6d97cab89da0f177e59eda062cb3d3
Shawn Westerlaken <shawnwesterlaken@gmail.com>
2020-11-18 14:15:32 +0000

    feat: :sparkles: day 1 part 1!

--- src/Advent/Days/Day1.fs
+++ src/Advent/Days/Day1.fs
@@ -1,7 +1,29 @@
 namespace Days
 
-module Day1 =
+open System.IO
 
-    let Part1 = ()
+module Day1 =
+    let getModuleWeights =
+        let readLines (filePath: string) =
+            seq {
+                use sr = new StreamReader(filePath)
+                while not sr.EndOfStream do
+                    yield sr.ReadLine()
+            }
+
+        let weights =
+            readLines "../../inputs/day1part1.txt"
+            |> Seq.map int
+
+        weights
+
+    let getRequiredFuel moduleWeight = moduleWeight / 3 - 2
+
+    let Part1 =
+        let sum =
+            Seq.sumBy getRequiredFuel getModuleWeights
+
+        printf $"Day 1 Part 1 Answer: {sum}\n"
+        ()
 
     let Part2 = ()
--- test/AdventTests/Days/Day1/Part1Tests.fs
+++ test/AdventTests/Days/Day1/Part1Tests.fs
@@ -3,9 +3,27 @@ namespace AdventTests.Days.Day1
 module Part1Tests =
 
     open NUnit.Framework
+    open Days.Day1
 
     [<SetUp>]
     let Setup () = ()
 
     [<Test>]
-    let Test1 () = Assert.Pass()
+    let Test1 () =
+        let result = getRequiredFuel 12
+        Assert.AreEqual(2, result)
+
+    [<Test>]
+    let Test2 () =
+        let result = getRequiredFuel 14
+        Assert.AreEqual(2, result)
+
+    [<Test>]
+    let Test3 () =
+        let result = getRequiredFuel 1969
+        Assert.AreEqual(654, result)
+
+    [<Test>]
+    let Test4 () =
+        let result = getRequiredFuel 100756
+        Assert.AreEqual(33583, result)
