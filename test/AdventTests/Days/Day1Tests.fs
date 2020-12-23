7f761e82421092ed170bf8605c00dee8bc9fa79f
Shawn Westerlaken <shawnwesterlaken@gmail.com>
2020-12-23 17:42:34 +0000

    chore: Update test proj to match src structure

--- test/AdventTests/Days/Day1Tests.fs
+++ test/AdventTests/Days/Day1Tests.fs
@@ -1,9 +1,25 @@
 namespace AdventTests.Days.Day1
 
-module Part2Tests =
+module Part1Tests =
 
     open NUnit.Framework
+    open FsUnit
     open Days.Day1
+
+    [<Test>]
+    let ``findExpensePair should solve example`` () =
+        // arrange
+        let target = 2020
+        let expenseReport = [ 1721; 979; 366; 299; 675; 1456 ]
+
+        // act
+        let result = findExpensePair target expenseReport
+
+        // assert
+        result |> should equal (Some(1721, 299))
+
+        let (a, b) = result.Value
+        a * b |> should equal 514579
 
     [<Test>]
     let ``findExpenseTriplet should solve example`` () =
