0d13ace8b6cbb75b900307da024880e1fce9214a
Gregory Bint <kabourneeak@gmail.com>
2021-03-21 16:27:33 -0400

    chore: day 3 stubs

---
+++ src/Advent/Days/Day3.fs
@@ -0,0 +1,14 @@
+namespace Days
+
+open Utils.FileUtils
+open System.Text.RegularExpressions
+
+module Day3 =
+
+    let Part1 =
+        printf $"Day 3 Part 1 Answer: Not Implemented\n"
+        ()
+
+    let Part2 = 
+        printf $"Day 3 Part 2 Answer: Not Implemented\n"
+        ()
--- src/Advent/Program.fs
+++ src/Advent/Program.fs
@@ -1,11 +1,4 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
 open Days
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
@@ -13,4 +6,6 @@ let main argv =
     Day1.Part2
     Day2.Part1
     Day2.Part2
+    Day3.Part1
+    Day3.Part2
     0 // return an integer exit code
